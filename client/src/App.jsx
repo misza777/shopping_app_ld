771410a9423a61412b9b7dfc1a5aa16a0b09c1c2
micha novakishvili <miau.nowak@gmail.com>
2023-01-11 17:20:34 +0100

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div className="app">
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </div>
+  );};
+
+function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <Home/> },
+    { path: "/products/:id", element: <Products/> },
+    { path: "/product/:id", element: <Product/> },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
